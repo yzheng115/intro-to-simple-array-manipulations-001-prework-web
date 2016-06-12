6645db0b87cb842306dca1ef3a7daf3062477a75
Yu Zheng <yzheng115@gmail.com>
2016-06-12 14:08:59 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,53 @@
+def using_push(array,element)
+  array.push(element)
+end
+
+def using_unshift(array,element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  temparray=[]
+  temparray.unshift(array.pop)
+  temparray.unshift(array.pop)
+  return temparray
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  temparray=[]
+  temparray<<array.shift
+  temparray<<array.shift
+  return temparray
+end
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,element)
+  array.delete(element)
+end
+
+def using_delete_at(array,number)
+  array.delete_at(number)
+end
